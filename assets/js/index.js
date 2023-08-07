c260c8ff201c4605171b3f19b9eb1759dfd101df
Marcelo Garita <mgaritaf@outlook.com>
2023-08-07 16:40:08 -0600

    adding scrolls

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,67 @@
+window.sr = ScrollReveal();
 
+sr.reveal(".header h1", {
+  duration: 3000,
+  origin: "right",
+  distance: "200px",
+});
+sr.reveal(".header h3", {
+  duration: 3500,
+  origin: "left",
+  distance: "200px",
+});
+sr.reveal(".Portfolio h2", {
+  duration: 4500,
+  origin: "bottom",
+  distance: "100px",
+});
+sr.reveal(".Portfolio .uno", {
+  duration: 4700,
+  origin: "right",
+  distance: "500px",
+});
+sr.reveal(".Portfolio .dos", {
+  duration: 5000,
+  origin: "left",
+  distance: "500px",
+});
+sr.reveal(".Portfolio .tres", {
+  duration: 5300,
+  origin: "right",
+  distance: "500px",
+});
+sr.reveal(".Portfolio .cuatro", {
+  duration: 5600,
+  origin: "left",
+  distance: "500px",
+});
+sr.reveal(".Portfolio .cinco", {
+  duration: 5900,
+  origin: "right",
+  distance: "500px",
+});
+sr.reveal(".Skillset h2", {
+  duration: 6000,
+  origin: "top",
+  distance: "100px",
+});
+sr.reveal(".skills", {
+  duration: 6900,
+  origin: "left",
+  distance: "1px",
+});
+sr.reveal(".Contact h2", {
+  duration: 6000,
+  origin: "top",
+  distance: "100px",
+});
+sr.reveal("form", {
+  duration: 7000,
+  origin: "right",
+  distance: "50px",
+});
+sr.reveal(".Contact .redes", {
+  duration: 9000,
+  origin: "bottom",
+  distance: "50px",
+});
